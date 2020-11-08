be6bbeeae37e9bb8f7cd5e4dcb63a48e0f0fcaf0
Филипп <filipp.schastny@ozitag.com>
2020-11-08 20:33:47 +0300

    !!Add starter!!

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+import './src/core/widget';
+import './src/core/observer';
+
+import './src/utils/helpers';
+import './src/utils/layout';
+import './src/utils/scroll-observer';
+import './src/utils/resize-observer';
+import './src/utils/scroll-control';
+import './src/utils/preloader';
+import './src/utils/vh-hack';
+import './src/utils/mobile-debug';
+
+import './src/widgets/popups';
+import './src/widgets/example';
+
+import './src/app';
+
