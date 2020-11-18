0be06d10d65706e99d4412171e0ac95c9b94f455
Vital Ozierski <v.ozierski@ozitag.com>
2020-11-18 17:34:53 +0300

    home video

--- app/js/src/widgets/home-students.js
+++ app/js/src/widgets/home-students.js
@@ -2,7 +2,11 @@ class HomeStudents extends Widget {
   constructor(node) {
     super(node, '.js-home-students');
 
+    this.swiper = null;
     this.$slider = this.queryElement('.slider');
+
+    this.$navPrev = this.queryElement('.prev');
+    this.$navNext = this.queryElement('.next');
 
     this.init();
   }
@@ -11,7 +15,18 @@ class HomeStudents extends Widget {
     this.swiper = new Swiper(this.$slider, {
       slidesPerView: 3,
       spaceBetween: 50,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        prevEl: this.$navPrev,
+        nextEl: this.$navNext,
+      },
     });
+  }
+
+  destroy() {
+    this.swiper.destroy(true, true);
   }
 
   static init(el) {
