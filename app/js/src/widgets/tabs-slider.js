09d4ef1dac40d99d604e1c1bd54d32e7e0c1c1aa
Vital Ozierski <v.ozierski@ozitag.com>
2020-11-19 22:59:04 +0300

    updates

--- app/js/src/widgets/tabs-slider.js
+++ app/js/src/widgets/tabs-slider.js
@@ -3,15 +3,18 @@ class TabsSlider extends Widget {
     super(node, '.js-tabs-slider');
 
     this.swiper = null;
-    this.$slider = this.queryElement('.slider');
+    this.activeFilter = null;
 
+    this.$slider = this.queryElement('.slider');
+    this.$slides = this.queryElements('.item');
     this.$navPrev = this.queryElement('.prev');
     this.$navNext = this.queryElement('.next');
+    this.$tabs = this.queryElements('.tab');
 
     this.init();
   }
 
-  build() {
+  initSwiper() {
     this.swiper = new Swiper(this.$slider, {
       slidesPerView: 4,
       spaceBetween: 10,
@@ -27,6 +30,81 @@ class TabsSlider extends Widget {
         slideChangeTransitionEnd: () => this.$slider.classList.remove('transition'),
       },
     });
+  }
+
+  events() {
+    this.$tabs.forEach($tab => $tab.addEventListener('click', e => {
+      e.preventDefault();
+      this.setActiveFilter($tab.dataset.filter);
+    }));
+  }
+
+  setActiveFilter(filter) {
+    this.activeFilter = filter;
+    this.$tabs.forEach($tab => $tab.classList.toggle('active', $tab.dataset.filter === filter));
+
+    this.processFilter();
+  }
+
+
+  showNode(node) {
+    node.classList.add('swiper-slide');
+    node.classList.remove('non-swiper-slide');
+  }
+
+  hideNode(node) {
+    node.classList.remove('swiper-slide');
+    node.classList.add('non-swiper-slide');
+  }
+
+  checkFilter(filter, $node) {
+    if (filter === '*') {
+      return true;
+    }
+
+    const filters = $node.dataset.filters ? $node.dataset.filters.split(',') : [];
+
+    return filters.indexOf(filter) !== -1;
+  }
+
+  processFilter() {
+    let visibleCount = 0;
+
+    this.$slides.forEach(node => {
+      if (this.checkFilter(this.activeFilter, node)) {
+        this.showNode(node);
+        visibleCount = visibleCount + 1;
+      } else {
+        this.hideNode(node);
+      }
+    });
+
+    this.swiper.destroy(true, true);
+
+    this.initSwiper();
+  }
+
+  default() {
+    let found = false;
+
+    this.$tabs.forEach($tab => {
+      if ($tab.classList.contains('active')) {
+        this.setActiveFilter($tab.dataset.filter);
+        found = true;
+      }
+    });
+
+    if (!found) {
+      this.setActiveFilter('*');
+    }
+  }
+
+  build() {
+    this.initSwiper();
+
+    this.events();
+
+    this.default();
   }
 
   destroy() {
