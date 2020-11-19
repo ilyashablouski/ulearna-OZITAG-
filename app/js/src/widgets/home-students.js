a251b6b6eab825d662b20928cd8f310247b1fc19
Vital Ozierski <v.ozierski@ozitag.com>
2020-11-19 23:50:37 +0300

    home featured

--- app/js/src/widgets/home-students.js
+++ app/js/src/widgets/home-students.js
@@ -3,10 +3,17 @@ class HomeStudents extends Widget {
     super(node, '.js-home-students');
 
     this.swiper = null;
+    this.sceneSwiper = null;
+
+    this.$scene = this.queryElement('.scene');
     this.$slider = this.queryElement('.slider');
 
     this.$navPrev = this.queryElement('.prev');
     this.$navNext = this.queryElement('.next');
+
+    this.$cells = this.queryElements('.cell');
+
+    this.switchTimer = null;
 
     this.init();
   }
@@ -14,11 +21,36 @@ class HomeStudents extends Widget {
   build() {
     this.swiper = new Swiper(this.$slider, {
       slidesPerView: 3,
-      spaceBetween: 50,
+      spaceBetween: 0,
       navigation: {
         prevEl: this.$navPrev,
         nextEl: this.$navNext,
       },
+    });
+
+    this.sceneSwiper = new Swiper(this.$scene, {
+      slidesPerView: 1,
+      effect: 'fade',
+      simulateTouch: false,
+      fadeEffect: {
+        crossFade: true,
+      },
+    });
+
+    this.$cells.forEach((node, ind) => {
+      node.addEventListener('mouseover', () => {
+        if (this.switchTimer) clearTimeout(this.switchTimer);
+        this.switchTimer = setTimeout(() => {
+          this.sceneSwiper.slideTo(ind);
+        }, 200);
+      });
+
+      node.addEventListener('mouseout', () => {
+        if (this.switchTimer) clearTimeout(this.switchTimer);
+        this.switchTimer = setTimeout(() => {
+          this.sceneSwiper.slideTo(0);
+        }, 200);
+      });
     });
   }
 
