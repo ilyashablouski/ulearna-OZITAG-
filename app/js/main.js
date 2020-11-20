492f7a6cd5b0552eca1fb04385e211f6a4b1b7c1
Vital Ozierski <v.ozierski@ozitag.com>
2020-11-20 14:26:15 +0300

    home featured

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,8 @@ import './src/utils/scroll-control';
 import './src/utils/preloader';
 import './src/utils/vh-hack';
 import './src/utils/mobile-debug';
+import './src/utils/scroll-to';
+import './src/utils/scroll-to-link';
 
 import './src/widgets/popups';
 
