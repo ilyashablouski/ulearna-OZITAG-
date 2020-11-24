ea9a7e3e2ea5d8d2c9e718ea2950996c358c017a
Vital Ozierski <v.ozierski@ozitag.com>
2020-11-24 20:00:04 +0300

    plan card

--- app/js/main.js
+++ app/js/main.js
@@ -11,9 +11,12 @@ import './src/utils/vh-hack';
 import './src/utils/mobile-debug';
 import './src/utils/scroll-to';
 import './src/utils/scroll-to-link';
+import './src/utils/input-filter';
 
 import './src/widgets/popups';
 import './src/widgets/accord';
+
+import './src/widgets/numeric-input';
 
 import './src/widgets/header/header-burger';
 import './src/widgets/header/header-fixed';
