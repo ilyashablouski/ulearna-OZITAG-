e335b43f93327c1751208ad59bdb83f3b9838a2f
Ilya Shablouski <ilya.shablouski@ozitag.com>
2020-12-26 13:50:26 +0300

    refactoring widget's markup

--- gulp/tasks/hbs.js
+++ gulp/tasks/hbs.js
@@ -21,6 +21,51 @@ module.exports = () => {
 
         return options.inverse(this);
       },
+      when: function(v1, operator, v2, options) {
+        switch (operator) {
+          case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+          case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+          case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+          case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+          case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+          case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+          case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+          case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+          case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+          case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+          default:
+            return options.inverse(this);
+        }
+      },
+      math: function(lvalue, operator, rvalue, options) {
+        if (arguments.length < 4) {
+          // Operator omitted, assuming "+"
+          options = rvalue;
+          rvalue = operator;
+          operator = "+";
+        }
+
+        lvalue = parseFloat(lvalue);
+        rvalue = parseFloat(rvalue);
+
+        return {
+          "+": lvalue + rvalue,
+          "-": lvalue - rvalue,
+          "*": lvalue * rvalue,
+          "/": lvalue / rvalue,
+          "%": lvalue % rvalue
+        }[operator];
+      },
       concat: function(...args) {
         return `${args.slice(0, -1).join('')}`;
       },
