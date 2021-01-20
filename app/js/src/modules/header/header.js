6bacce1934a91625f4dd25f9142d3049f9f3df1b
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-01-20 14:19:03 +0300

    bugfix add sticky-header

--- app/js/src/modules/header/header.js
+++ app/js/src/modules/header/header.js
@@ -2,10 +2,10 @@ class Header extends Widget {
   constructor(node) {
     super(node, 'js-header');
 
-    HeaderFixed.init(this.$node);
     HeaderBurger.init(this.$node);
     HeaderMenu.init(this.$node);
     HeaderSearch.init(this.$node);
+    HeaderFixed.init(this.$node);
   }
 
   static init(el) {
