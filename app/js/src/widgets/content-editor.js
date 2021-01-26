86a52adad4ccff7c80a1bd3fc909aeaf174fb969
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-01-26 15:52:38 +0300

    add editable functionality on quiz page

--- app/js/src/widgets/content-editor.js
+++ app/js/src/widgets/content-editor.js
@@ -1,25 +1,61 @@
 class ContentEditor extends Widget {
   constructor(nodeElement) {
     super(nodeElement, '.js-content-editor');
-    // this.editButtons = this.queryElements('.js-content-edit-btn');
 
     this.init();
   }
 
   build() {
-    this.$node.addEventListener('click', e => {
-      const target = e.target;
-      console.log(target);
-      if (target.classList.contains('js-content-edit-btn')) {
-        // this.editElement();
-        console.log('Деллигированный обработчик сработал');
+    document.addEventListener('click', e => {
+      const clickedElement = e.target;
+      const editableElement = clickedElement.previousElementSibling;
+      const notEditElement = !clickedElement.classList.contains('js-content-edit-btn') && !clickedElement.hasAttribute('contenteditable');
+      if (clickedElement.classList.contains('js-content-edit-btn')) {
+        this.onEditElement(editableElement);
+      } else if (notEditElement) {
+        this.onAnchorElements();
       }
     });
   }
 
-  // editElement () {
-  //
-  // }
+  onEditElement(element) {
+    element.setAttribute('contenteditable', true);
+    this.setCustomRangeColor(element);
+    this.setCaret(element);
+  }
+
+  onAnchorElements() {
+    const editableElements = this.$node.querySelectorAll('span');
+    const editableElementsArray = Object.values(editableElements);
+
+    const filteredElements = editableElementsArray.filter(element => {
+      return element.hasAttribute('contenteditable');
+    });
+
+    for (const filteredElement of filteredElements) {
+      filteredElement.setAttribute('contenteditable', false);
+      this.removeCustomRangeColor(filteredElement);
+    }
+  }
+
+  setCaret(element) {
+    const childNode = element.firstChild;
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNodeContents(childNode);
+    range.collapse(false);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+
+  setCustomRangeColor(rangeElement) {
+    rangeElement.classList.add('selection');
+  }
+
+  removeCustomRangeColor(rangeElement) {
+    rangeElement.classList.remove('selection');
+  }
 
   static init(element) {
     new ContentEditor(element);
