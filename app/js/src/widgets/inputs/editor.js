3d395b2be429cfe2165d7aec47418dd1ca5e935d
Ilya Shablouski <ilya.shablouski@ozitag.com>
2021-02-02 15:45:39 +0300

    bugfix add buttons header to editor

--- app/js/src/widgets/inputs/editor.js
+++ app/js/src/widgets/inputs/editor.js
@@ -1,23 +1,17 @@
 class Editor extends Widget {
   constructor(element) {
     super(element, '.editor');
+    this.initEditorPlugin();
   }
 
-  static init() {
+  initEditorPlugin() {
     ClassicEditor.create(document.querySelector('.editor'), {
       toolbar: ['bold', 'italic', 'bulletedList', 'numberedList', '|', 'heading'],
       heading: {
         options: [
-          {
-            model: 'heading2',
-            view: 'h2',
-            title: '<svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-              '<path d="M7.56363 0.601318C7.72236 0.236184 8.08257 0 8.48072 0H10.4698C10.8671 0 11.2267 0.235171 11.386 0.599131L18.387 16.5991C18.6761 17.2598 18.1921 18 17.4709 18H15.1707C14.7646 18 14.3989 17.7544 14.2451 17.3786L12.9394 14.1864H5.90713L4.64847 17.3679C4.49753 17.7494 4.12888 18 3.7186 18H1.52513C0.804983 18 0.320931 17.2618 0.60804 16.6013L7.56363 0.601318ZM9.35935 5.23729L7.16016 10.8305H11.5841L9.35935 5.23729Z" fill="#20B7F7"/>\n' +
-              '</svg>\n',
-            class: 'editor__heading editor__heading--2',
-          },
-          { model: 'heading3', view: 'h3', title: '', class: 'editor__heading editor__heading--3' },
-          { model: 'heading4', view: 'h4', title: '', class: 'editor__heading editor__heading--4' },
+          { model: 'heading2', view: 'h2', title: 'A', class: 'editor__heading editor__heading--2' },
+          { model: 'heading3', view: 'h3', title: 'A', class: 'editor__heading editor__heading--3' },
+          { model: 'heading4', view: 'h4', title: 'A', class: 'editor__heading editor__heading--4' },
         ],
       },
     })
@@ -27,13 +21,31 @@ class Editor extends Widget {
         new PerfectScrollbar($editorScrollElement, {
           minScrollbarLength: 20,
         });
+        // Change default heading
+        this.changeHeading();
       })
       .catch(error => {
         console.error(error);
       });
-
   }
 
+  changeHeading() {
+    const headingElements = document.querySelectorAll('.editor__heading');
+    const panelElement = document.querySelector('.ck-toolbar__items');
+    headingElements.forEach(element => {
+      panelElement.appendChild(element);
+      element.innerHTML = `<svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4.75505 0.984125C4.91331 0.617948 5.27407 0.380859 5.67298 0.380859H6.29528C6.69333 0.380859 7.05348 0.616935 7.21227 0.981944L11.2665 10.3014C11.4102 10.6316 11.1681 11.0009 10.808 11.0009H9.26721C9.06381 11.0009 8.88067 10.8776 8.80405 10.6892L8.01578 8.75086H3.89078L3.13046 10.6839C3.05524 10.8751 2.87066 11.0009 2.66516 11.0009H1.18658C0.826967 11.0009 0.584946 10.6326 0.727618 10.3025L4.75505 0.984125ZM5.91578 3.47086L4.62578 6.77086H7.22078L5.91578 3.47086Z" fill="currentColor"/>
+      </svg>
+      `;
+
+    });
+    console.log(headingElements);
+  };
+
+  static init(element) {
+    new Editor(element);
+  }
 }
 
 
